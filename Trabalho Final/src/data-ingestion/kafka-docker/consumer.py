da564f6aee5566befc115506c1674a53fdd1b81d
fernandopassoss <luisfernandopassos84@gmail.com>
2025-08-06 23:34:23 -0300

    inserindo consumer

--- Trabalho Final/src/data-ingestion/kafka-docker/consumer.py
+++ Trabalho Final/src/data-ingestion/kafka-docker/consumer.py
@@ -1,18 +1,19 @@
 from kafka import KafkaConsumer
 import json
 
-# Initialize KafkaConsumer
+
+KAFKA_TOPIC = 'mercado_financeiro'
+KAFKA_BROKER = 'kafka:9092'
 
 consumer = KafkaConsumer(
-    'temperature_sensor_topic', 
-    bootstrap_servers='kafka:9092', 
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-
-    group_id='temperature_sensor_topic_group', # Consumer group ID
-    value_deserializer=lambda x: json. loads(x.decode('utf-8'))
+    KAFKA_TOPIC,
+    bootstrap_servers=KAFKA_BROKER,
+    value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+    auto_offset_reset='earliest',  # pega desde o início do tópico
+    group_id='grupo_consumidor_financeiro',  # necessário pra Kafka controlar o offset
+    enable_auto_commit=True
 )
 
 if __name__ == '__main__':
     for message in consumer:
-        print(f"Received: {message. value}")
+        print(f"Received: {message.value}")
